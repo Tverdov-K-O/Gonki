406c0c360bda1b9add68e80f6e7cf973c2c702b3
tverdov.konstantin tverdov.konstantin <konstantin08081997@mail.ru>
2021-10-19 08:45:36 +0300

    Добавьте файлы проекта.

---
+++ Cars.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gonki
+{
+    abstract class Car // Автомобиль
+    {
+        public int Number { get; set; }         // номер автомобиля
+        public int Speed { get; set; }          // скорость автомобиля
+        public int Distance { get; set; } = 0;  // дистанция которую проехал
+        public abstract void Ready();           // метод готовности автомобиля который вызывается через событие "carsRady"
+        public abstract int Startrace();        // начало движения автомобиля
+        public abstract void Winner();          // метод на случай победы автомобиля который вызывает событие "Winner"
+    }
+
+    class SportCar : Car // Спорткар
+    {
+        public SportCar(int N) { this.Number = N; }
+        public override void Ready()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Спорткар готов к гонке!");
+        }
+
+        public override int Startrace()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            int value = new Random().Next(1, 6);
+            this.Distance += value;
+            Console.Write($"Спорткар\t№[{this.Number}] дистанция: {Distance}km\t|Start|");
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = 0; i < this.Distance/2; i++){ Console.Write("-"); }
+            Console.Write("S");
+            for (int i = 0; i < 50 - this.Distance/2; i++) { Console.Write(" "); }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("|Finish|");
+            return Distance;
+        }
+        public override void Winner()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\nЯ - {this} №[{this.Number}] и я победил!");
+        }
+        public override string ToString()
+        {
+            return "Спорткар";
+        }
+
+        
+    }
+    class LiteCar : Car // Легковой автомобиль
+    {
+        public LiteCar(int N) { this.Number = N; }
+        public override void Ready()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Легковушка готова к гонке!");
+        }
+
+        public override int Startrace()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            int value = new Random().Next(2, 5);
+            this.Distance += value;
+            Console.Write($"Легковушка\t№[{this.Number}] дистанция: {Distance}km\t|Start|");
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < this.Distance / 2; i++) { Console.Write("-"); }
+            Console.Write("L");
+            for (int i = 0; i < 50 - this.Distance / 2; i++) { Console.Write(" "); }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("|Finish|");
+            return Distance;
+        }
+        public override void Winner()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Я - {this} №[{this.Number}] и я победил!");
+        }
+        public override string ToString()
+        {
+            return "Легковой автомобиль";
+        }
+    }
+    class Truck : Car // Грузовик
+    {
+        public Truck(int N) { this.Number = N; }
+        public override void Ready()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Грузовик готов к гонке!");
+        }
+        public override int Startrace()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            int value = new Random().Next(3, 4);
+            this.Distance += value;
+            Console.Write($"Грузовик\t№[{this.Number}] дистанция: {Distance}km\t|Start|");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            for (int i = 0; i < this.Distance / 2; i++) { Console.Write("-"); }
+            Console.Write("T");
+            for (int i = 0; i < 50 - this.Distance / 2; i++) { Console.Write(" "); }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("|Finish|");
+            return Distance;
+        }
+        public override void Winner()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Я - {this} №[{this.Number}] и я победил!");
+        }
+        public override string ToString()
+        {
+            return "Грузовик";
+        }
+    }
+    class Bus : Car // Автобус
+    {
+        public Bus(int N) { this.Number = N; }
+        public override void Ready()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Автобус готов к гонке!");
+        }
+
+        public override int Startrace()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            int value = new Random().Next(3, 4);
+            this.Distance += value;
+            Console.Write($"Автобус\t\t№[{this.Number}] дистанция: {Distance}km\t|Start|");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < this.Distance / 2; i++) { Console.Write("-"); }
+            Console.Write("B");
+            for (int i = 0; i < 50 - this.Distance / 2; i++) { Console.Write(" "); }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("|Finish|");
+            return Distance;
+        }
+        public override void Winner()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Я - {this} №[{this.Number}] и я победил!");
+        }
+        public override string ToString()
+        {
+            return "Автобус";
+        }
+    }
+
+
+}
---
+++ Race.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gonki
+{
+
+    public delegate void Cars();    // делегат без параметров
+    class Race
+    {
+        public int Distance { get; set; } = 100;    // дистанция 100км
+        List<Car> Cars = new List<Car>();           // список Автомобилей в гонке
+        public event Cars carsRady; // событие о готовности Автомобилей к гонке!
+        public event Cars Winner;   // событие о победителе!
+        int NumberCar = 0;          // номер машины!
+        public Race() { }           // конструктор без параметров
+        public void StartRace()     // гонка
+        {
+            carsRady();
+            Console.ReadKey(); Console.Clear(); Console.ForegroundColor = ConsoleColor.White;
+            while (Winner == null) 
+            {
+                System.Console.SetCursorPosition(0, 0);
+                System.Console.CursorVisible = false;
+                foreach (var item in Cars)
+                {
+                    item.Startrace();
+                    if(item.Distance >= this.Distance && Winner == null)
+                    {
+                        Winner += item.Winner;
+                    }
+                }
+                System.Threading.Thread.Sleep(200);
+            }
+            Winner();
+            Console.ReadKey();
+        }
+        public void Menu()          // главное меню
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Clear();
+                Console.WriteLine("\t\t-Гонки-");
+                Console.WriteLine(" 1 - Добавить в гонку Легковушку");
+                Console.WriteLine(" 2 - Добавить в гонку Спорткар ");
+                Console.WriteLine(" 3 - Добавить в гонку Грузовик");
+                Console.WriteLine(" 4 - Добавить в гонку Автобус");
+                Console.WriteLine(" 5 - Начать гонку");
+                Console.WriteLine(" 6 - Выйти");
+                int action = Convert.ToInt32(Console.ReadLine());
+                Car car= null;
+                switch (action)
+                {
+                    case 1:
+                        car = new LiteCar(++NumberCar);
+                        Cars.Add(car);
+                        carsRady += car.Ready;
+                        break;
+                    case 2:
+                        car = new SportCar(++NumberCar);
+                        Cars.Add(car);
+                        carsRady += car.Ready;
+                        break;
+                    case 3:
+                        car = new Truck(++NumberCar);
+                        Cars.Add(car);
+                        carsRady += car.Ready;
+                        break;
+                    case 4:
+                        car = new Bus(++NumberCar);
+                        Cars.Add(car);
+                        carsRady += car.Ready;
+                        break;
+                    case 5:
+                        StartRace();
+                        exit = true;
+                        break;
+                    case 6:
+                        exit = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
